3420fe023bfbea022da659f6b3c5613833cd969d
Yekon08 <max1901@live.fr>
2018-05-16 11:36:30 +0200

    test

--- CV/JS.js
+++ CV/JS.js
@@ -3,12 +3,12 @@ function afficher_cacher(id)
     if(document.getElementById(id).style.visibility=="hidden")
     {
         document.getElementById(id).style.visibility="visible";
-        document.getElementById('bouton_'+id).innerHTML='Cacher le texte';
+        document.getElementById('bouton_'+id).innerHTML='Mes coordonnees';
     }
     else
     {
         document.getElementById(id).style.visibility="hidden";
-        document.getElementById('bouton_'+id).innerHTML='Afficher le texte';
+        document.getElementById('bouton_'+id).innerHTML='Mes coordonnees';
     }
     return true;
 }
