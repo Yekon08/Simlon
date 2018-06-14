302f29fc1afa7b91114c9f53a15821fb03b021c5
Yekon08 <max1901@live.fr>
2018-06-14 14:28:08 +0200

    exo 5

---
+++ JS/exo 5/main.js
@@ -0,0 +1,11 @@
+var monDiv = document.querySelector("div");
+
+console.log(monDiv);
+
+monDiv.setAttribute("id","test");
+
+console.log(monDiv);
+
+monDiv.className = "classe";
+
+console.log(monDiv);
